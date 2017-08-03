b2d879bf1e0f6c72292121485b9bf8ca6fd534b2
adriana19-meet <adriana19@meet.mit.edu>
2017-08-03 14:33:19 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -5,11 +5,20 @@ turtle.tracer(1,0)
 SIZE_X=800
 SIZE_Y=500
 turtle.setup(SIZE_X, SIZE_Y)
-
+counter=1
 turtle.penup()
 
 SQUARE_SIZE = 20
 START_LENGTH = 1
+
+box=turtle.clone()
+box.goto(0,250)
+box.pendown()
+box.goto(400,250)
+box.goto(-400,250)
+box.goto(-400,-250)
+box.goto(400,-250)
+box.goto(400,250)
 
 
 pos_list = []
@@ -59,20 +68,24 @@ direction = UP
 
 def up():
     global direction
-    direction=UP
-    print("you pressed the up key!")
+    if direction != DOWN:
+        direction=UP
+        print("you pressed the up key!")
 def down():
     global direction
-    direction=DOWN
-    print ("you pressed the down key!")
+    if direction !=UP:
+         direction=DOWN
+         print ("you pressed the down key!")
 def left():
     global direction
-    direction=LEFT
-    print("you pressed the left key!")
+    if direction !=RIGHT:
+        direction=LEFT
+        print("you pressed the left key!")
 def right():
     global direction
-    direction=RIGHT
-    print("you pressed the right key!")
+    if direction !=LEFT:
+        direction=RIGHT
+        print("you pressed the right key!")
 
 turtle.onkeypress(up, UP_ARROW)
 turtle.onkeypress(down, DOWN_ARROW)
@@ -81,17 +94,23 @@ turtle.onkeypress(right, RIGHT_ARROW)
 turtle.listen()
 
 def make_food():
-    min_x=-int(SIZE_X/2/SQUARE_SIZE)+1
-    max_x=int(SIZE_X/2/SQUARE_SIZE)-1
-    min_y=-int(SIZE_Y/2/SQUARE_SIZE)+1
-    max_y=int(SIZE_Y/2/SQUARE_SIZE)-1
+    min_x=-int(SIZE_X/2.5/SQUARE_SIZE)+1
+    max_x=int(SIZE_X/2.5/SQUARE_SIZE)-1
+    min_y=-int(SIZE_Y/2.5/SQUARE_SIZE)+1
+    max_y=int(SIZE_Y/2.5/SQUARE_SIZE)-1
 
     food_x = random.randint(min_x,max_x)*SQUARE_SIZE
     food_y = random.randint(min_y,max_y)*SQUARE_SIZE
+    new_food_pos = (food_x, food_y)
+
+    if new_food_pos in pos_list: # food made on snake
+        make_food()
+    
     food.goto(food_x,food_y)
     food_pos.append(food.pos())
     new_food = food.stamp()
     food_stamps.append(new_food)
+
 
 def move_snake():
     my_pos = snake.pos()
@@ -116,12 +135,20 @@ def move_snake():
     stamp_list.append(new_stamp)
     global food_stamps, food_pos
     if snake.pos() in food_pos:
+        global counter
         food_ind=food_pos.index(snake.pos())
         food.clearstamp(food_stamps[food_ind])
         food_pos.pop(food_ind)
         food_stamps.pop(food_ind)
+        counter=counter+1
+        turtle.clear()
+        turtle.write(counter,font=(30
+                                   ))
         print("you have eaten the food!")
         make_food()
+        
+
+        
 
     else:
         #deletes the last stamp
@@ -156,16 +183,4 @@ make_food()
 
 
 
-         
 
-
-   
-    
-        
-        
-    
-    
-    
-
-
-    
